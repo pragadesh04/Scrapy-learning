426b236372fce74a5017cbfdd3a69f3d25b9f249
Praga866 <pragaofficial004@gmail.com>
2024-07-31 21:14:43 +0530

    better Scrapping

--- CreepyPastas/CreepyPastas/spiders/CP.py
+++ CreepyPastas/CreepyPastas/spiders/CP.py
@@ -1,5 +1,5 @@
 from typing import Iterable
-
+import time
 
 import scrapy
 
@@ -11,23 +11,32 @@ class CpSpider(scrapy.Spider):
     sorted = "/archive/top-ranked/"
 
     allowed_domains = [dn]
-    start_urls = ["https://www.creepypasta.com" + sorted + "?_page=1"]
+    # start_urls = ["https://www.creepypasta.com" + sorted + "?_page=1"]
     scope = []
-    for i in range (0,4):
+    for i in range (1,2):
         scope.append("https://"+ dn + sorted + "?_page=" + str(i))
 
     def start_requests(self):
-        urls = self.start_urls + self.scope
+        time.sleep(0.05)
+        urls =self.scope
         for url in urls:
             yield scrapy.Request(url = url, callback=self.parse)
 
     def parse(self, response):
-        div = response.css("._self.cvplbd::text").extract()
+        # div = response.css("._self.cvplbd::text").extract()
+        div = response.css(".pt-cv-ifield")
+        title = div.css("._self.cvplbd img::attr(alt)").extract()
+        links = div.css(".pt-cv-href-thumbnail::attr(href)").extract()
+        rating = div.css(".pt-cv-ctf-value::text").extract()
+
         with open("Creppies.txt" , 'a') as file:
-            for i in range(len(div)):
-                if div[i] == "Read Now":
-                    continue
-                else:
-                    file.write(div[i] + "\n")
-            file.write("\n\nPowered by www.CreepyPasta.com")
+            for i in range(len(title)):
+                file.write("Title:" + title[i] + "\n")
+                file.write("Links: " + links[i] + "\n" + f"Average Rating: {rating[i]}".center(len(links[i])+7, "-") + "\n\n")
+
+        yield{
+            "div" : div,
+            "title": title,
+            "links": links
+        }
         pass
