eb4aa07e489e6587ede3048c36551836780906b5
Praga866 <pragaofficial004@gmail.com>
2024-07-31 08:03:00 +0530

    collected quotes

--- quotes/quotes/spiders/quotesSpider.py
+++ quotes/quotes/spiders/quotesSpider.py
@@ -7,4 +7,19 @@ class QuotesspiderSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com"]
 
     def parse(self, response):
+        div = response.css(".quote")
+        quotes = div.css(".text::text").extract()
+        author = div.css(".author::text").extract()
+
+        file = open("quotes.txt", "w")
+        i = 0
+        
+        for t in quotes:
+            file.write("\n" + t + "\n" + author[i] + '\n')
+            i += 1
+
+        yield{
+            'quotes': quotes,
+            'author': author,
+        }
         pass
